53b48d0e3dd996c4ff957404d9792c4cea1c9185
hissoune <khalidhissoune962@gmail.com>
2024-11-07 10:15:25 +0100

    Add : adding the user service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthImplementation } from '../repository/implementations/auth.implementation';
+import { UserEntity } from '../entities/user.entity';
+import { UserDocument } from '../schemas/user.schema';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly authImplementation: AuthImplementation) {}
+
+  async register(user: UserEntity): Promise<UserDocument> {
+    return this.authImplementation.register(user);
+  }
+
+ async login(user: UserEntity): Promise<{token:string}>{
+
+  return  this.authImplementation.login(user);
+ }
+
+ async verifyToken(token: string) {
+  try {
+    return await this.authImplementation.verifyToken(token);
+  } catch (error) {
+    throw new UnauthorizedException('Invalid or expired token');
+  }
+}
+}
