758ac71b8ea6da19b79e66f1bd0e6df120775cab
hissoune <khalidhissoune962@gmail.com>
2024-11-07 10:12:45 +0100

    Add : adding the user interface

---
+++ src/repository/interfaces/auth.interface.ts
@@ -0,0 +1,11 @@
+import { UserDocument } from "../../schemas/user.schema";
+import { UserEntity } from "../../entities/user.entity";
+
+export interface AuthInterface {
+    register(user: UserEntity): Promise<UserDocument>;
+
+
+    login(user: UserEntity): Promise< {token: string} >;
+
+    
+}
