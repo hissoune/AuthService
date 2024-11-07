b3318c44fac772f9b383c479373d96c57d3bfbfd
hissoune <khalidhissoune962@gmail.com>
2024-11-07 10:16:35 +0100

    Add : adding the controller of the user and create the endpoints of the login and the register

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,51 @@
+import { Body, Controller, Get, Headers, Post, UnauthorizedException } from "@nestjs/common";
+import { AuthService } from "../services/auth.service";
+import { UserEntity } from "../entities/user.entity";
+import { UserDocument } from "../schemas/user.schema";
+
+@Controller("auth")
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post("register")
+  async register(
+    @Body()
+    body: {
+      name: string;
+      email: string;
+      password: string;
+      phone: number;
+    }
+  ): Promise<UserDocument> {
+    const userEntity = new UserEntity(
+      body.name,
+      body.email,
+      body.password,
+      body.phone
+    );
+
+    return this.authService.register(userEntity);
+  }
+
+  @Post("login")
+  async login(
+    @Body() body: { email: string; password: string }
+  ): Promise<{token:string}> {
+    const userEntity = new UserEntity(body.email, body.password);
+    return this.authService.login(userEntity)
+  }
+
+  @Get('verify')
+  async verifyToken(
+    @Headers('authorization') authorization: string
+  ): Promise<UserDocument> {
+    if (!authorization) {
+      throw new UnauthorizedException('No token provided');
+    }
+
+    const token = authorization.split(' ')[1];
+    return this.authService.verifyToken(token);
+  }
+
+  
+}
