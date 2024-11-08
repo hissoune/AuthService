6194974621b48686e2e5b79acd8f3074df89c819
hissoune <khalidhissoune962@gmail.com>
2024-11-08 11:56:31 +0100

    Fix : sixing the user scema and and the table of friends for the funtionality of accepting friends

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,4 +1,4 @@
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema()
@@ -11,9 +11,13 @@ export class User extends Document {
 
     @Prop({ required: true })
     password: string;
-
+    
     @Prop({ required: true })
     phone: number;
+
+    @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }])
+    friends?:  mongoose.Schema.Types.ObjectId[];
+
 }
 
 
