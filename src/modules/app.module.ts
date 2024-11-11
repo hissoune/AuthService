a3fbf66909ef9b2759d9ebd355d47185f976ae26
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-11 13:46:56 +0100

    Feat --> Adding forgotPassword method and adding resetPasword method and installing package of @nest/nodemailer

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/AuthService'),
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: +process.env.SMTP_PORT,
+        secure: false,
+        auth: {
+          user: process.env.APP_EMAIL,
+          pass: process.env.APP_EMAIL_PASS,
+        },
+      },
+      defaults: {
+        from: `"${process.env.APP_EMAIL_USERNAME}" <${process.env.APP_EMAIL}>`,
+      },
+    }),
+
 
     AuthModule
   ],
--- src/repository/interfaces/auth.interface.ts
+++ src/repository/interfaces/auth.interface.ts
@@ -4,10 +4,9 @@ import { UserEntity } from "../../entities/user.entity";
 export interface AuthInterface {
     register(user: UserEntity): Promise<UserDocument>;
 
+    login(user: UserEntity): Promise<{ token: string }>;
 
-    login(user: UserEntity): Promise< {token: string} >;
+    forgotPassword(email: string): Promise<{ email: string }>;
 
-   
-
-    
+    resetPassword(resetToken: string, newPassword: string): Promise<{ message: string }>;
 }
